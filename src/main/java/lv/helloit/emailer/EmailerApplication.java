9ae414dbb48a269f5168b5f663ac6022a1faaa74
Dan Fridman <dan.fridman@yahoo.com>
2020-01-27 19:50:32 +0200

    Initial commit

---
+++ src/main/java/lv/helloit/emailer/EmailerApplication.java
@@ -0,0 +1,13 @@
+package lv.helloit.emailer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailerApplication.class, args);
+	}
+
+}
